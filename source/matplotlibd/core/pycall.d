e81f34b4c43225bce08b010905ed909b6c1aead4
koji-kojiro <kojiro0531@gmail.com>
2016-12-25 21:59:20 +0900

    Improve maintainability of pyplot.d

---
+++ source/matplotlibd/core/pycall.d
@@ -0,0 +1,25 @@
+module matplotlibd.core.pycall;
+
+void call(string py_script) {
+    import std.process: environment, pipeProcess, wait, Redirect;
+
+    auto py_path =  environment.get("PYTHON", "python");
+    auto pipes = pipeProcess(py_path, Redirect.stdin | Redirect.stderr);
+
+    pipes.stdin.writeln(py_script);
+    pipes.stdin.writeln("exit()");
+    pipes.stdin.close();
+
+    auto result = wait(pipes.pid);
+
+    if (result != 0) {
+        string error;
+        foreach (line; pipes.stderr.byLine)
+            error ~= line ~ "\n";
+        throw new Exception("\n\nERROR occurred in Python:\n" ~ error);
+    }
+}
+
+unittest {
+    call("print(\"Passing!\")\n");
+}
